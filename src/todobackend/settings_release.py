e1d7f5e87dc8f8b8e44e2202567556ccc58bf0d9
Samir Gadkari <samir.gadkari@gmail.com>
2021-11-18 18:05:50 -0800

    Commented code

--- src/todobackend/settings_release.py
+++ src/todobackend/settings_release.py
@@ -13,10 +13,16 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 # Database settings
 DATABASES = {
     'default': {
+        # Override the default SQLite driver with the MySQL
+        # connector
         'ENGINE': 'mysql.connector.django',
+
+        # These values are set to the same values in the docker-compose.yml file,
+        # and are the defaults when the given environment variables are not defined.
         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
         'USER': os.environ.get('MYSQL_USER','todo'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+
         'HOST': os.environ.get('MYSQL_HOST','localhost'),
         'PORT': os.environ.get('MYSQL_PORT','3306'),
     },
@@ -25,5 +31,8 @@ DATABASES = {
     }
 }
 
+# Django will look for static content in the /public/static directory of the container
+# by default, if the STATIC_ROOT env variable is not defined.
 STATIC_ROOT = os.environ.get('STATIC_ROOT', '/public/static')
+
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/public/media')
